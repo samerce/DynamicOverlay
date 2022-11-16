9c1445cfe6eb0f5450b888e6af1730d39117ad62
bubbles sandcastle <bsandcastle@ironox.com>
2022-11-16 15:07:36 -0500

    switched to expandableHeight

---
+++ Source/Internal/OverlayContainer/OverlayContainerRepresentableAdaptator.swift
@@ -0,0 +1,95 @@
+//
+//  OverlayContainerRepresentableAdaptator.swift
+//  DynamicOverlayTests
+//
+//  Created by Gaétan Zanella on 20/04/2021.
+//  Copyright © 2021 Fabernovel. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import OverlayContainer
+
+struct OverlayContainerRepresentableAdaptator<Content: View, Background: View> {
+
+    class Context {
+        let coordinator: OverlayContainerCoordinator
+        let transaction: Transaction
+
+        init(coordinator: OverlayContainerCoordinator, transaction: Transaction) {
+            self.coordinator = coordinator
+            self.transaction = transaction
+        }
+    }
+
+    let searchsScrollView: Bool
+    let handleValue: DynamicOverlayDragHandle
+    let behavior: DynamicOverlayBehaviorValue
+    let content: Content
+    let background: Background
+
+    private var animationController: DynamicOverlayContainerAnimationController {
+        DynamicOverlayContainerAnimationController()
+    }
+
+    private var containerState: OverlayContainerState {
+        OverlayContainerState(
+            searchsScrollView: searchsScrollView,
+            notchIndex: behavior.binding?.wrappedValue,
+            disabledNotches: behavior.disabledNotchIndexes,
+            layout: OverlayContainerLayout(indexToDimension: behavior.notchDimensions ?? [:])
+        )
+    }
+
+    // MARK: - UIViewControllerRepresentable
+
+    func makeCoordinator() -> OverlayContainerCoordinator {
+        let contentController = UIHostingController(rootView: content)
+        contentController.view.backgroundColor = .clear
+        let backgroundController = UIHostingController(rootView: background)
+        backgroundController.view.backgroundColor = .clear
+        return OverlayContainerCoordinator(
+            layout: containerState.layout,
+            animationController: animationController,
+            background: backgroundController,
+            content: contentController
+        )
+    }
+
+    func makeUIViewController(context: Context) -> OverlayContainerViewController {
+        let controller = OverlayContainerViewController(style: .expandableHeight)
+        controller.delegate = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: OverlayContainerViewController, context: Context) {
+        context.coordinator.notchChangeUpdateHandler = { notch in
+            behavior.binding?.wrappedValue = notch
+        }
+        context.coordinator.translationUpdateHandler = { coordinator in
+            let animation = animationController.animation(using: coordinator)
+            let transaction = Transaction(animation: animation)
+            let translation = OverlayTranslation(
+                height: coordinator.targetTranslationHeight,
+                transaction: transaction,
+                isDragging: coordinator.isDragging,
+                translationProgress: coordinator.overallTranslationProgress(),
+                containerFrame: uiViewController.view.frame,
+                velocity: coordinator.velocity,
+                heightForNotchIndex: { coordinator.height(forNotchAt: $0) }
+            )
+            withTransaction(transaction) {
+                behavior.block?(translation)
+            }
+        }
+        context.coordinator.shouldStartDraggingOverlay = { container, point, coordinateSpace in
+            guard let overlay = container.topViewController else { return false }
+            let inOverlayPoint = overlay.view.convert(point, from: coordinateSpace)
+            if handleValue.spots.isEmpty {
+                return overlay.view.frame.contains(inOverlayPoint)
+            }
+            return handleValue.contains(inOverlayPoint)
+        }
+        context.coordinator.move(uiViewController, to: containerState, animated: context.transaction.animation != nil)
+    }
+}
